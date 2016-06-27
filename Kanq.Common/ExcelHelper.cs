8868765d082ead2addcf595e6ed33ec3ce21bc72
Saber <Saber@DESKTOP-3JAA4NM>
2016-06-27 20:21:40 +0800

    添加项目文件。

---
+++ Kanq.Common/ExcelHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace Kanq.Common
+{
+    public class ExcelHelper
+    {
+        public static MemoryStream ExportExcel()
+        {
+            //创建Excel工作薄
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            //创建单元格样式
+            ICellStyle cellTitleStyle = workbook.CreateCellStyle();
+            //设置单元格居中显示
+            cellTitleStyle.Alignment = HorizontalAlignment.Center;
+            //创建字体
+            IFont font = workbook.CreateFont();
+            //设置字体加粗显示
+            font.IsBold = true;
+            cellTitleStyle.SetFont(font);
+            //创建Excel工作表
+            ISheet sheet = workbook.CreateSheet("管理员");
+            //创建Excel行
+            IRow row = sheet.CreateRow(0);
+            //创建Excel单元格
+            ICell cell = row.CreateCell(0);
+            //设置单元格值
+            cell.SetCellValue("管理员管理");
+            //设置单元格合并
+            sheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(0, 0, 0, 3));
+            cell.CellStyle = cellTitleStyle;
+            //using (FileStream fs = new FileStream(@"C:\Users\Saber\Desktop\Demo.xlsx", FileMode.OpenOrCreate))
+            //{
+            //    workbook.Write(fs);
+            //}
+            MemoryStream ms;
+            using (ms = new MemoryStream())
+            {
+                workbook.Write(ms);
+            }
+            return ms;
+        }
+    }
+}
---
+++ Kanq.Web/ExportExcel.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kanq.Common;
+using System.IO;
+
+namespace Kanq.Web
+{
+    /// <summary>
+    /// ExportExcel 的摘要说明
+    /// </summary>
+    public class ExportExcel : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.AppendHeader("Content-Disposition", "attachment;filename=demo.xlsx");// 下载后的文件名
+            MemoryStream ms = ExcelHelper.ExportExcel();
+            context.Response.BinaryWrite(ms.ToArray());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
